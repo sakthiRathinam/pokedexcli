fb814c41293e59b8462f6d700233468808ce14f2
sakthi <sakthiratnam050@gmail.com>
2024-07-01 00:19:58 +0530

    chore: minor change in exit command

--- command_exit.go
+++ command_exit.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/sakthiRathinam/pokedexcli/internal/pokedex"
 )
 func CommandExit(cfg *pokedex.PokedexConfig) error{
 	fmt.Printf("pokedexcli is shutting down")
-	return errors.New("Quit!!!!!!")
+	return nil
 }
