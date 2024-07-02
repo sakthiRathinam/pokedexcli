14d56a6e25308cf91698c261dfcbc2d21b245754
sakthi <sakthiratnam050@gmail.com>
2024-07-02 21:30:20 +0530

    chore: added two more command on the help section

--- command_help.go
+++ command_help.go
@@ -9,9 +9,11 @@ func DisplayHelpText(cfg *pokedex.PokedexConfig) error{
 	fmt.Printf(
         "Welcome to pokedexcli! These are the available commands: \n",
     )
-    fmt.Println(".help    - Show available commands")
-    fmt.Println(".clear   - Clear the terminal screen")
-    fmt.Println(".exit    - Closes the terminal screen")
+    fmt.Println("help    - Show available commands")
+    fmt.Println("clear   - Clear the terminal screen")
+    fmt.Println("exit    - Closes the terminal screen")
+    fmt.Println("map     - Shows the locations on the pokemon world")
+    fmt.Println("mapb    - Goback to previous locations on the pokemon world")
 	return nil
 }
 
